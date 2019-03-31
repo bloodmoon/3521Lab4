9dd73ba1d7375b9952d8ac9749f0faf0a1ec37bf
batey.18 <batey.18@osu.edu>
2019-03-31 18:38:28 -0400

    Only iteration loop and output logic left

--- ValueIterator.py
+++ ValueIterator.py
@@ -1,16 +1,38 @@
-from collections import defaultdict
-import IterAgentUtils
 import sys
+from mdp import MDP
 
-# Setup Variables
-transition_matrix = defaultdict(dict)
-action_list = []
-state_reward_dict = {}
 
+def value_iteration(mdp_in, epsilon):
+
+    # Setup Variables
+    current_util = {s: 0 for s in mdp_in.state_reward_dict}
+
+    while True:
+        adjusted_util = current_util.copy()
+        conv_dist = 0
+
+        for s in mdp_in.state_reward_dict:
+            maximum = 0
+            for a in mdp_in.get_actions():
+                # TODO: Figure out how to multiply probability by utility
+                for probability in mdp_in.get_state_transition_prob_list(s, a):
+                    # maximum = max(sum(probability * current))
+                    print(probability)
+
+            current_util[s] = mdp_in.get_reward(s) + mdp_in.get_gamma() * maximum
+
+            conv_dist = max(conv_dist, abs(current_util[s] - adjusted_util[s]))
+
+        if conv_dist < epsilon:
+            return adjusted_util
+
+
+# Get file names from command line args
 state_file = sys.argv[1]
 transition_file = sys.argv[2]
 
-IterAgentUtils.parse_state_file(state_file, state_reward_dict)
-IterAgentUtils.parse_actions_probs_file(transition_file, action_list, transition_matrix)
+# Generate MDP
+mdp = MDP(state_file, transition_file)
+value_iteration(mdp, 0.000006)
 
-print(state_reward_dict)
+
--- mdp.py
+++ mdp.py
@@ -1,7 +1,47 @@
 """A Markov Decision Process Module"""
 
+from collections import defaultdict
+import IterAgentUtils
+
 
 class MDP:
 
-    def __init__(self, init_state, ):
-        print("FINISH INITIALIZATION")
+    def __init__(self, state_file, transition_file):
+        self.transition_matrix = defaultdict(dict)
+        self.action_list = []
+        self.state_reward_dict = {}
+        IterAgentUtils.parse_state_file(state_file, self.state_reward_dict)
+        IterAgentUtils.parse_actions_probs_file(transition_file, self.
+                                                action_list, self.transition_matrix)
+        self.discount = 0
+        self.gamma = 1
+
+    def get_reward(self, state):
+        return self.state_reward_dict[state]
+
+    def get_actions(self):
+        return self.action_list
+
+    def get_discount(self):
+        return self.discount
+
+    def get_transition_matrix(self):
+        return self.transition_matrix
+
+    def get_state_transition_prob_list(self, state, action):
+        if self.is_terminal(state):
+            # TODO : PROB  SHOULD JUST RETURN ZERO TO MAKE UNCHANGED
+            tuplist = list()
+            tuplist.append(tuple((self.get_reward(state), 1.0)))
+            return tuplist
+        else:
+            return self.transition_matrix[state][action]
+
+    def is_terminal(self, state):
+        if abs(self.state_reward_dict[state]) == 1:
+            return True
+        else:
+            return False
+
+    def get_gamma(self):
+        return self.gamma
