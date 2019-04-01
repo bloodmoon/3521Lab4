4469bb5a9a3ea56435c6c2b1d4f75ade5ffa2817
batey.18 <batey.18@osu.edu>
2019-03-31 22:32:42 -0400

    ValueIterator Implementation complete

--- ValueIterator.py
+++ ValueIterator.py
@@ -5,23 +5,35 @@ from mdp import MDP
 def value_iteration(mdp_in, epsilon):
 
     # Setup Variables
-    current_util = {s: 0 for s in mdp_in.state_reward_dict}
+    current_util = {st: 0 for st in mdp_in.state_reward_dict}
 
     while True:
         adjusted_util = current_util.copy()
         conv_dist = 0
 
-        for s in mdp_in.state_reward_dict:
-            maximum = 0
+        for state in mdp_in.state_reward_dict:
+            maximum = []
+            action_sum_list = []
             for a in mdp_in.get_actions():
-                # TODO: Figure out how to multiply probability by utility
-                for probability in mdp_in.get_state_transition_prob_list(s, a):
-                    # maximum = max(sum(probability * current))
-                    print(probability)
+                # List to hold calculated probabilities of each action
+                curr_sum = 0
 
-            current_util[s] = mdp_in.get_reward(s) + mdp_in.get_gamma() * maximum
+                for probability in mdp_in.get_state_transition_prob_list(state, a):
+                    res_state = probability[0]
+                    if res_state is not 0:
+                        curr_sum = curr_sum + (probability[1] * adjusted_util[probability[0]])
 
-            conv_dist = max(conv_dist, abs(current_util[s] - adjusted_util[s]))
+                # Add to sum list for finding max and store resultant state for finding policy
+                action_sum_list.append(tuple((curr_sum, a)))
+
+            maximum.append(max(action_sum_list))
+
+            # Update lists/dicts/policy
+            current_util[state] = mdp_in.get_reward(state) + mdp_in.get_gamma() * maximum[0][0]
+            mdp_in.set_policy(state, maximum[0][1])
+
+            # Find the distance between the two values on a number line
+            conv_dist = max(conv_dist, abs(current_util[state] - adjusted_util[state]))
 
         if conv_dist < epsilon:
             return adjusted_util
@@ -33,6 +45,12 @@ transition_file = sys.argv[2]
 
 # Generate MDP
 mdp = MDP(state_file, transition_file)
-value_iteration(mdp, 0.000006)
+utility = value_iteration(mdp, 0.0001)
+
+for s in utility:
+    print("State ", s, ":", "  Utility:", utility[s], "  Policy: ", mdp.get_policy(s))
+
+
+
 
 
--- mdp.py
+++ mdp.py
@@ -15,6 +15,7 @@ class MDP:
                                                 action_list, self.transition_matrix)
         self.discount = 0
         self.gamma = 1
+        self.policy_dict = {}
 
     def get_reward(self, state):
         return self.state_reward_dict[state]
@@ -30,7 +31,6 @@ class MDP:
 
     def get_state_transition_prob_list(self, state, action):
         if self.is_terminal(state):
-            # TODO : PROB  SHOULD JUST RETURN ZERO TO MAKE UNCHANGED
             tuplist = list()
             tuplist.append(tuple((0, 0)))
             return tuplist
@@ -45,3 +45,16 @@ class MDP:
 
     def get_gamma(self):
         return self.gamma
+
+    def set_policy(self, state, policy):
+        if self.is_terminal(state):
+            self.policy_dict[state] = 'Terminal State'
+        elif policy is 'S':
+            self.policy_dict[state] = 'Scarlet'
+        elif policy is 'G':
+            self.policy_dict[state] = 'Grey'
+        elif policy is 'B':
+            self.policy_dict[state] = 'Black'
+
+    def get_policy(self, state):
+        return self.policy_dict[state]
