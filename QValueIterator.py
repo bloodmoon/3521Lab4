02fd0bbfaa7e06fa7fe2adc231dc32361fc240c4
batey.18 <batey.18@osu.edu>
2019-03-31 23:42:13 -0400

    ValueIterator Implementation complete

--- QValueIterator.py
+++ QValueIterator.py
@@ -2,31 +2,28 @@ import sys
 from mdp import MDP
 
 
-def value_iteration(mdp_in, epsilon):
+def qvalue_iteration(mdp_in, epsilon):
 
     # Setup Variables
-    current_util = {st: 0 for st in mdp_in.state_reward_dict}
+    qval_list = {st: tuple((0, 0)) for st in mdp_in.state_reward_dict}
 
     while True:
-        adjusted_util = current_util.copy()
         conv_dist = 0
+        adj_qval_list = qval_list.copy()
 
         for state in mdp_in.state_reward_dict:
-            maximum = []
-            action_sum_list = []
             for a in mdp_in.get_actions():
+
                 # List to hold calculated probabilities of each action
                 curr_sum = 0
 
                 for probability in mdp_in.get_state_transition_prob_list(state, a):
                     res_state = probability[0]
                     if res_state is not 0:
-                        curr_sum = curr_sum + (probability[1] * adjusted_util[probability[0]])
+                        curr_sum = curr_sum + (probability[1] * adj_qval_list[probability[0]])
 
                 # Add to sum list for finding max and store resultant state for finding policy
-                action_sum_list.append(tuple((curr_sum, a)))
-
-            maximum.append(max(action_sum_list))
+                qval_list.append(tuple((curr_sum, a)))
 
             # Update lists/dicts/policy
             current_util[state] = mdp_in.get_reward(state) + mdp_in.get_gamma() * maximum[0][0]
@@ -48,7 +45,7 @@ eps = float(input("Please enter an epsilon value that you would like to use: "))
 
 # Generate MDP
 mdp = MDP(state_file, transition_file)
-utility = value_iteration(mdp, eps)
+utility = qvalue_iteration(mdp, eps)
 
 for s in utility:
     print("State", s, ":", " Utility:", utility[s], "  Policy: ", mdp.get_policy(s))
